4aa4346543f3b1bb96a67a6bf4f1cd65ec03958f
Daniel Cousens <github@dcousens.com>
2018-09-13 17:22:18 +1000

    add failing tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,28 @@
-var test = require('tape')
-var hoodwink = require('../')
+const tape = require('tape')
+const hoodwink = require('../')
 
-test('', function (t) {
+tape('explodes on too few calls', function (t) {
+  t.throws(() => {
+    hoodwink(function () {
+      function f () {}
+      const stub = this.stub(f, 3)
+
+      stub()
+    })()
+  }, /Too few calls/)
+
+  t.end()
+})
+
+tape('explodes on too many calls', function (t) {
+  hoodwink(function () {
+    function f () {}
+    const stub = this.stub(f, 2)
+
+    stub()
+    stub()
+    t.throws(() => stub())
+  }, /Too many calls/)()
+
   t.end()
 })
